a525db1aaf629340a1485bf026b005446031a39a
zimisele <zimisele9@gmail.com>
2024-07-04 14:43:11 +0200

    add extra property

--- main.tf
+++ main.tf
@@ -4,13 +4,13 @@ resource "aws_instance" "ec2" {
   subnet_id = ""
   security_groups = [ "" ]
   tags = {
-    Name = "Demo"
+    Name = "practise"
   }
 }
 #we need to change instance type
 resource "aws_security_group" "example" {
   # ... other configuration ...
-
+    vpc_id = ""
   egress {
     from_port        = 0
     to_port          = 0
