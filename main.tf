5632b26938237d6139447ebeb80c7be08ef60f86
zimisele <43851011+zimisele@users.noreply.github.com>
2024-07-04 14:55:44 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ resource "aws_security_group" "example" {
   #nana need to be add aswell
   #this thing is working just fine
   egress {
-    from_port       = 0
-    to_port         = 0
+    from_port       = 80
+    to_port         = 80
     protocol        = "-1"
     prefix_list_ids = [aws_vpc_endpoint.my_endpoint.prefix_list_id]
   }
