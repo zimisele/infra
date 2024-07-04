906ba46e2c26fc91d2f7679ecd5d350396bbd7ab
zimisele <zimisele9@gmail.com>
2024-07-04 14:40:51 +0200

    added security groups

--- main.tf
+++ main.tf
@@ -8,3 +8,14 @@ resource "aws_instance" "ec2" {
   }
 }
 #we need to change instance type
+resource "aws_security_group" "example" {
+  # ... other configuration ...
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
