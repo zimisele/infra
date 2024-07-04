6985ca70decba5a2e9a87dbf073726f7537df2c9
zimisele <zimisele9@gmail.com>
2024-07-04 14:55:17 +0200

    third

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_security_group" "example" {
   # ... other configuration ...
   #nana need to be add aswell
+  #this thing is working just fine
   egress {
     from_port       = 0
     to_port         = 0
