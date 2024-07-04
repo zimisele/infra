fe071c127fa79def7908ea8310858c5e65503625
zimisele <zimisele9@gmail.com>
2024-07-04 14:51:35 +0200

    first add

--- main.tf
+++ main.tf
@@ -1,21 +1,10 @@
-resource "aws_instance" "ec2" {
-  instance_type = "t2.micro"
-  ami = ""
-  subnet_id = "newSubnets"
-  security_groups = [ "" ]
-  tags = {
-    Name = "Zimisele"
-  }
-}
-#we need to change instance type
 resource "aws_security_group" "example" {
   # ... other configuration ...
-    vpc_id = ""
+
   egress {
-    from_port        = 80
-    to_port          = 80
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    prefix_list_ids = [aws_vpc_endpoint.my_endpoint.prefix_list_id]
   }
 }
