ec8ad28f980335b142d9e0632181b8b91b7ccffb
zimisele <zimisele9@gmail.com>
2024-07-04 16:55:19 +0200

    second

--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">=1.0.2"
+  required_version = "~> 1.3"
   required_providers {
     aws = {
         source = "hashicorp/aws"
-        version = "3.74.2"
+        version = "~> 4.0"
     }
   }
 }
