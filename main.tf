00e426f97f96f2dc82317ea50f7dc60bba35fa67
zimisele <zimisele9@gmail.com>
2024-07-04 14:44:56 +0200

    newsubnets

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   instance_type = "t2.micro"
   ami = ""
-  subnet_id = ""
+  subnet_id = "newSubnets"
   security_groups = [ "" ]
   tags = {
     Name = "practise"
@@ -12,8 +12,8 @@ resource "aws_security_group" "example" {
   # ... other configuration ...
     vpc_id = ""
   egress {
-    from_port        = 0
-    to_port          = 0
+    from_port        = 80
+    to_port          = 80
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
