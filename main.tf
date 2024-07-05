5d0eeef278d5f7f7573670a77527bf47c42ec734
zimisele <zimisele9@gmail.com>
2024-07-05 11:37:03 +0200

    added ec2 instance

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
 module "demo" {
   source = "./modules/ec2F"
-  instance_type = "t3.large"
+  instance_type = "t3.micro"
+}
+module "prod" {
+ source = "./modules/ec2F"
+ instance_type = "t3.large"
 }
 
 module "myBucket" {
