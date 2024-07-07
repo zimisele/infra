b6e34e3e9bad29d6fa9966eded1dfb67beb99fda
zimisele <zimisele9@gmail.com>
2024-07-07 11:18:14 +0200

    adding resources

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-# module "demo" {
-#   source = "./modules/ec2F"
-#   instance_type = "t3.micro"
-# }
-# module "prod" {
-#  source = "./modules/ec2F"
-#  instance_type = "t3.large"
-#  ami = "ami-0c38b837cd80f13bb"
-# }
+module "demo" {
+  source = "./modules/ec2F"
+  instance_type = "t3.micro"
+}
+module "prod" {
+ source = "./modules/ec2F"
+ instance_type = "t3.large"
+ ami = "ami-0c38b837cd80f13bb"
+}
 
-# module "myBucket" {
-#   source = "./modules/s3"
-#   bucket = "Nana4232123"
-# }
+module "myBucket" {
+  source = "./modules/s3"
+  bucket = "Nana4232123"
+}
