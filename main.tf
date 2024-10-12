4288cef23e599bf9f118bd8da17171c3fc78972c
zimisele <zimisele9@gmail.com>
2024-10-12 10:18:34 +0200

    commented

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@
 #   source = "./modules/ec2F"
 #   instance_type = "t3.micro"
 # }
-# module "prod" {
-#  source = "./modules/ec2F"
-#  instance_type = "t3.large"
-#  ami = "ami-0c38b837cd80f13bb"
-# }
-
+module "prod" {
+ source = "./modules/ec2F"
+ instance_type = "t3.large"
+ ami = "ami-0c38b837cd80f13bb"
+}
+# 
 # module "myBucket" {
 #   source = "./modules/s3"
 #   bucket = "Nana4232123"
